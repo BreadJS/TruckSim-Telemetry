1107e1333425d87cb2d64d12ad2aa6f4dab29f88
BreadJS <devvcat@protonmail.com>
2026-04-06 17:46:05 +0200

    fix: use local typescript to ensure version 5.9.3 is used
    Also add node_modules/.bin to PATH for install script.

--- scripts-install.js
+++ scripts-install.js
@@ -4,7 +4,8 @@ const path = require('path');
 // Run build from the package directory
 const buildProcess = spawn('npm', ['run', 'build'], {
   stdio: 'inherit',
-  cwd: __dirname
+  cwd: __dirname,
+  env: { ...process.env, PATH: path.join(__dirname, 'node_modules', '.bin') + ':' + process.env.PATH }
 });
 
 buildProcess.on('exit', (code) => {
